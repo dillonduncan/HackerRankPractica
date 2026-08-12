9d8b0f93a7614fbf48d36a090240382ed022787d
Dillon Duncan <dillonduncan0607@gmail.com>
2026-08-11 20:53:33 -0500

    dc y mc

--- HackerRankPractica/Repasos/RepasoDatosLINQ.cs
+++ HackerRankPractica/Repasos/RepasoDatosLINQ.cs
@@ -3,6 +3,8 @@
     public record Jugador(string Nombre, string Posicion, int Valoracion, string Equipo, int Velocidad, int Tiro);
     public class RepasoDatosLINQ
     {
+        public record Anonimo(string Nombre, int Potencial);
+
         public void Ejecutar()
         {
             var plantilla = new List<Jugador>
@@ -17,9 +19,19 @@
                 new Jugador("Willer Ditta", "DEF", 76, "Cruz Azul", 78, 40)
             };
             //Delanteros con val mayor a 80
+            Console.WriteLine("Delanteros con valoracion > 80:");
             plantilla.Where(j => j.Posicion == "DEL" && j.Valoracion > 80)
+                .OrderByDescending(j => j.Valoracion)
                 .ToList()
                 .ForEach(j => Console.WriteLine($"{j.Nombre} - {j.Valoracion}"));
+
+            //Coleccion de mediocampistas
+            Console.WriteLine("\nPotencial Ofensivo de los Mediocampistas:");
+            List<Jugador> mediocampistas = plantilla.Where(j => j.Posicion == "MED").ToList();
+            var anonimos = mediocampistas.Select(j => new Anonimo(j.Nombre, (j.Valoracion + j.Velocidad + j.Tiro) / 3));
+            anonimos.ToList().ForEach(a => Console.WriteLine($"{a.Nombre} - {a.Potencial}"));
+
+
         }
 
     }
